64c86124e6b63588679beda7ad1c574d0d4a4b76
Masanori Mitsugi <mitsugi@linux.vnet.ibm.com>
2022-01-05 12:38:37 +0900

    Update save_model.py

--- scripts/save_model.py
+++ scripts/save_model.py
@@ -22,6 +22,9 @@ from tensorflow.keras.applications.xception import Xception
 if len(sys.argv) < 2:
     print('Usage: python %s model_type' % sys.argv[0])
     exit(-1)
+    
+model_type = sys.argv[1]
+print('Running: %s model_type=%s' % (sys.argv[0], model_type))
 
 if model_type == "xception":
     model = Xception(weights='imagenet')
